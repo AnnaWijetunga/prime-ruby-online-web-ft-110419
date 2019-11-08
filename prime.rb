7b1cc54492c7264af1064b1fac6a9e2d3e431b89
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 17:48:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(number)
-  if number <= 1 
-    return false
-  elsif n <= 3
-    return true
-  else (2..n/2).none? do |x|
-    n % x == 0 
+  start = 2 
+  if number > 1 
+    range = (start..number-1).to_a range.none? do |num_to_test|
+      number % num_to_test == 0 
+    end
+  else
+    false
   end
 end
 
