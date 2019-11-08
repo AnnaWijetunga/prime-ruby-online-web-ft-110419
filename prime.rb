414c36635e6d67ab0b229b3017a4b54905ad47e0
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 17:45:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,13 @@
 def prime?(number)
   if number <= 1 
     return false
-  elsif n <=
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0 
+  end
+  end
+end
 
   
   #takes in an integer argument
