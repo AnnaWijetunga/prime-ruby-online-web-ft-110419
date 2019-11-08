6cbafbaa2b5bed02d54a39bce272a85d1f4ce066
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 17:27:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
-def prime?( )
-  
+def prime?(number)
+  start = 2 
+  if number > 1 
+    range = (start..number-1).to_a range.non? do |num_to_test|
   
   #takes in an integer argument
   #returns a boolean of whether or not that integer is a prime number
