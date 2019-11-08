0faefa19e7cff95adb29aa66ee7c6d08fcb8812e
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 17:19:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,6 +5,7 @@ def prime?( )
   #takes in an integer argument
   #returns a boolean of whether or not that integer is a prime number
   #what defines a prime number?
+  #A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. A natural number greater than 1 that is not prime is called a composite number.
   #how to create a range of numbers?
   #how to turn a range into an array so it can be iterated over?
   #don't use any other Ruby library
