82a140772519ce49d4755381d229b22234aebf39
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 17:14:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,10 @@
 def prime?( )
   #takes in an integer argument
   #returns a boolean of whether or not that integer is a prime number
+  #what defines a prime number?
+  #how to create a range of numbers?
+  #how to turn a range into an array so it can be iterated over?
+  #don't use any other Ruby library
+  #must build a method that can verify whether a number is prime
+  #don't require 'math' 
 end
